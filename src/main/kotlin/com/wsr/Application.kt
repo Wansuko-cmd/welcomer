956888628bed37a58810205a1203a70a2e1f3ff6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-06 22:50:23 +0900

    [add] rootのポストの動作を追加

--- src/main/kotlin/com/wsr/Application.kt
+++ src/main/kotlin/com/wsr/Application.kt
@@ -1,15 +1,25 @@
 package com.wsr
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.main(){
+
+    install(ContentNegotiation){
+        json()
+    }
+
     routing {
         post("/"){
-            call.respondText("Hello World!")
+            val challenge = call.receive<Challenge>()
+
+            call.respond(challenge)
         }
     }
 }
