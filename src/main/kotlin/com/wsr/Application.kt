3c34ea75ef8faa59fb3d57dc6922fbd1ec7f42f2
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-06 13:24:47 +0900

    first commit

---
+++ src/main/kotlin/com/wsr/Application.kt
@@ -0,0 +1,12 @@
+package com.wsr
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.wsr.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
