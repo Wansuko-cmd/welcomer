3e2abec6f18ba756c7d18ae34e4bf7502bf9e908
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-07 00:27:59 +0900

    [add] チャンネルに投稿する機能を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/main/kotlin/com/wsr/routings/Route.kt
+++ src/main/kotlin/com/wsr/routings/Route.kt
@@ -1,19 +1,43 @@
 package com.wsr.routings
 
+import com.typesafe.config.ConfigFactory
 import com.wsr.entities.Challenge
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.config.*
+import kotlinx.coroutines.launch
 
 fun Application.route(){
+    val client = HttpClient(CIO)
+    val appConfig = HoconApplicationConfig(ConfigFactory.load())
+    val url = appConfig.property("slack.url").getString()
+
     routing {
         get("/"){
             call.respondText("This server is working!")
+
+            val job = launch {
+                val response: HttpResponse = client.post(url) {
+                    body = """{"text": "Hello World"}"""
+                }
+            }
+            job.join()
         }
 
         post("/"){
             val challenge = call.receive<Challenge>()
+            val job = launch {
+                val response: HttpResponse = client.post(url) {
+                    body = """{"text": "Hello World"}"""
+                }
+            }
+            job.join()
 
             call.respond(challenge)
         }
